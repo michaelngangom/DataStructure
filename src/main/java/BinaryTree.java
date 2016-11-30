f4a6d54876486dcf6f983284afa8b2ea50f5d5b7
Michael Ngangom <michael.ngangom@gmail.com>
2016-11-30 21:40:15 +0100

    BT2

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -51,13 +51,63 @@ public class BinaryTree {
 
     }
 
+    //pre order tree traversal
+    public void preOrderTraversalRecursive(Node focusNode)
+    {
+        if(focusNode!=null)
+        {
+            System.out.println(focusNode);
+            preOrderTraversalRecursive(focusNode.leftChild);
+
+            preOrderTraversalRecursive(focusNode.rightChild);
+        }
+
+    }
+//Post order traversal
+    public void postOrderTraversalRecursive(Node focusNode)
+    {
+        if(focusNode!=null)
+        {
+
+            postOrderTraversalRecursive(focusNode.leftChild);
+
+            postOrderTraversalRecursive(focusNode.rightChild);
+            System.out.println(focusNode);
+        }
+
+    }
+
+    public Node searchNode(int key)
+    {
+        Node focusNode=root;
+        while(focusNode.key != key)
+        {
+            if(key<focusNode.key)
+                focusNode = focusNode.leftChild;
+            else {
+                focusNode = focusNode.rightChild;
+            }
+            if(focusNode ==null)
+            return null;
+        }
+return focusNode;
+    }
+
     public static void main(String[] args){
         BinaryTree tree=new BinaryTree();
         tree.addNode(10,"Mike");
-        tree.addNode(20,"Tom");
+        tree.addNode(5,"Tom");
         tree.addNode(30,"Jack");
 
-        tree.inOrderTraversalRecursive(tree.root);
+     //   tree.preOrderTraversalRecursive(tree.root);
+
+     //   tree.postOrderTraversalRecursive(tree.root);
+
+
+       // tree.preOrderTraversalRecursive(new Node(5,"Tom"));
+
+
+        System.out.println( tree.searchNode(30));
     }
 }
 
@@ -75,7 +125,7 @@ class Node
 
     public String toString()
     {
-        return name + "has a key" + key;
+        return name + " has a key " + key;
     }
 
         }
